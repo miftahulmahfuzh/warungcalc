f8a37025b26f6441179740522069543dd11682f1
miftahulmahfuzh <miftahulmahfuzh@gmail.com>
2016-02-16 12:10:55 +0700

    section 2 done

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
-var result = null; //used to saving the result, or the left side of operation
+var result = null; //saving the result, or the left side of operation
 var rightside = null;
-var text = ""; //used to save temporary result
+var text = ""; //saving the input 
 var operator = ""; 
 var pressed = false; //operator flag
 function append(x) {
@@ -18,14 +18,13 @@ function operate(x) {
 	} else if(pressed && rightside==null) {//do nothin
 	} else if(pressed) {
 	   //case if the input is: 'x (operator) y (operator)' 
-	   rightside = parseInt(text);
 	   result(); //it will do '=' function
 	   operator = x;
 	   text = "";
 	   pressed = true;
 	}
 	else { //case of input: 'x (operator)'
-	   result = parseInt(text);
+	   if(text != "") result = parseInt(text);
 	   operator = x;
 	   text = ""; //reinitialize text for rightside
 	   pressed = true;	
@@ -34,7 +33,7 @@ function operate(x) {
 
 function maths(operator,x,y) {
 	switch(operator) {
-	   case 'x': return x * y;
+	   case '*': return x * y;
 	   case '/': return x / y;
 	   case '+': return x + y;
 	   default : return x - y;
@@ -48,17 +47,16 @@ function getresult() {
 	   rightside = parseInt(text);
 	   result = maths(operator,result,rightside);
 	   document.getElementById("lcd").value = result.toString();
-	   rightside = null; //reinitialize rightside
-	   pressed = false; //reinitialize operator flag
+	   rightside = null; 
+	   pressed = false; 
+	   text = "";
 	}
 }
 
 function clearit() { //reinitialize everything
-	console.log("print berhasil");
 	document.getElementById("lcd").value = "";
 	result = null;
 	rightside = null;
 	text = "";
 	pressed = false;
-	return false;
 }
